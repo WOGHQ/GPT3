fa0a3fca1d7bd6dcad365df12343708b895fece5
WOGHQ <danieljbetancourt4@gmail.com>
2024-03-27 01:14:47 -0700

    Migrated contents from one repo to another.

---
+++ src/containers/header/Header.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import './header.css'
+import people from '../../assets/people.png'
+import ai from '../../assets/ai.png'
+
+const Header = () => {
+  return (
+    <div className='gpt3__header section__padding' id='home'>
+      <div className='gpt3__header-content'>
+        <h1 className='gradient__text'>
+          Let's Build Something amazing with GPT-3 OpenAI
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, distinctio cupiditate dolores sit dolorem nisi, ex nihil, optio inventore sapiente ullam recusandae voluptatibus deserunt modi placeat eaque fuga provident nemo!
+        </p>
+
+        <div className='gpt3__header-content__input'>
+          <input type='email' placeholder='Your Email Address' />
+          <button type='button'>Get Started</button>
+        </div>
+
+        <div className='gpt3__header-content__people'>
+          <img src={people} />
+          <p>1,600 people requested access within the last 24 hours</p>
+        </div>        
+      </div>
+      <div className='gpt3__header-image'>
+        <img src={ai} alt='ai'/>
+      </div>
+    </div>
+  )
+}
+
+export default Header
